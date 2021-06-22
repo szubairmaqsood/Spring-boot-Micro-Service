b2830ba0cf5cd6726d061188e52760705212b962
zubair-pc\zubair <szubair@gmail.com>
2021-06-22 17:54:50 +0500

    Euraka client is set up in movie Catalog project

--- Movie Catalog Service/Movie-Catalog-Service/build.gradle.kts
+++ Movie Catalog Service/Movie-Catalog-Service/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.0.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- Movie Catalog Service/Movie-Catalog-Service/src/main/kotlin/com/example/Movie/Catalog/Service/MovieCatalogServiceApplication.kt
+++ Movie Catalog Service/Movie-Catalog-Service/src/main/kotlin/com/example/Movie/Catalog/Service/MovieCatalogServiceApplication.kt
@@ -2,12 +2,14 @@ package com.example.Movie.Catalog.Service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
 @Configuration
+@EnableEurekaClient
 class MovieCatalogServiceApplication
 
 @Bean
