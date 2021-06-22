228bed82e31ce921b69b3d8b018cb227ca65e508
zubair-pc\zubair <szubair@gmail.com>
2021-06-22 17:37:58 +0500

    In movie info and in movie rating Eruka client is added

--- Rating Data Service/Rating-Data-Service/src/main/kotlin/com/example/Rating/Data/Service/RatingDataServiceApplication.kt
+++ Rating Data Service/Rating-Data-Service/src/main/kotlin/com/example/Rating/Data/Service/RatingDataServiceApplication.kt
@@ -2,8 +2,10 @@ package com.example.Rating.Data.Service
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class RatingDataServiceApplication
 
 fun main(args: Array<String>) {
