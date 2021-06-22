25ea87e30c9921f113e436282417b720aa5817c8
zubair-pc\zubair <szubair@gmail.com>
2021-06-22 15:51:18 +0500

    Eruka server is made again

---
+++ Eruka-Server/src/main/kotlin/com/example/ErukaServer/ErukaServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ErukaServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ErukaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ErukaServerApplication>(*args)
+}
