c46304e2fcb94a2f60136969a5dcac0a3d66a2da
zubair-pc\zubair <szubair@gmail.com>
2021-06-22 15:38:25 +0500

    Eruka Server project is added with Eruka server dependency

---
+++ Eruka Server/Eruka-Server/src/main/java/com/example/Eruka/Server/ErukaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Eruka.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErukaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErukaServerApplication.class, args);
+	}
+
+}
---
+++ Eruka Server/Eruka-Server/src/test/java/com/example/Eruka/Server/ErukaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eruka.Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
