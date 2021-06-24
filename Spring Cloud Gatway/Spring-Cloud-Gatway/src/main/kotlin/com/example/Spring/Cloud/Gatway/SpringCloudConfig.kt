80bd64a3c5f19bd947de9abfd75f8a79b566ded9
zubair-pc\zubair <szubair@gmail.com>
2021-06-24 12:51:20 +0500

    Path is added to application.properties and path is got using @value

--- Spring Cloud Gatway/Spring-Cloud-Gatway/src/main/kotlin/com/example/Spring/Cloud/Gatway/SpringCloudConfig.kt
+++ Spring Cloud Gatway/Spring-Cloud-Gatway/src/main/kotlin/com/example/Spring/Cloud/Gatway/SpringCloudConfig.kt
@@ -1,5 +1,6 @@
 package com.example.Spring.Cloud.Gatway
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
@@ -7,21 +8,32 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SpringCloudConfig {
+
+    @Value("\${movie.Path}")
+    var movie: String?=null;
+
+    @Value("\${ratings.Path}")
+     var rating: String?=null;
+
+    @Value("\${catalog.Path}")
+    var catalog:String? =null;
+
+
     @Bean
     fun myRoutes(builder: RouteLocatorBuilder): RouteLocator? {
         return builder.routes()
             .route { r ->
                 r.path("/catalog/**")
-                    .uri("http://localhost:8084")
+                    .uri(catalog)
             }
 
             .route { r ->
                 r.path("/movies/**")
-                    .uri("http://localhost:8081")
+                    .uri(movie)
             }
             .route { r ->
                 r.path("/ratingsdata/**")
-                    .uri("http://localhost:8082")
+                    .uri(rating)
             }
             .build()
     }
